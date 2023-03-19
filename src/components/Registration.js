197b971993ac193db6e7b8eb4ab6f5e1a16709c8
vas1levnv <vas1levnv@yandex.ru>
2023-03-19 21:49:44 +0300

    popup

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -82,9 +82,31 @@ function Registration() {
                     <label className="form-check-label"
                            htmlFor="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit"
+                        className="btn btn-primary">Submit</button>
             </form>
-
+            <button
+                    data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    className="btn btn-primary">Submitsadfgsa</button>
+            <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel"
+                 aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
