9e337f5a6569d7b5ae802b09875396ec62f67d9e
vas1levnv <vas1levnv@yandex.ru>
2023-03-21 12:29:25 +0300

    удаление поста

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,14 +4,16 @@ import s from './Posts.module.scss'
 const Posts = () => {
 
     const [posts, setPosts] = useState(null)
-
-
     useEffect(() => {
 
         fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
             .then(response => response.json())
             .then(response => setPosts(response))
     }, [])
+
+    const removePost = (p) => {
+        setPosts(posts.filter((post) => post.id !== p))
+    }
 
 
     return (
@@ -20,7 +22,11 @@ const Posts = () => {
                 {posts && posts.map(p => (
                     <div className={s.postItem} key={p.id}>
                         <div>{p.id}</div>
-                        <div className={s.postItemTitle}>{p.title}</div>
+                        <div>
+                            <div className={s.postItemTitle}>{p.title}</div>
+                            <div className={s.postItemBody}>{p.body}</div>
+                        </div>
+                        <button onClick={() => removePost(p.id)}>Удалить</button>
                     </div>
                 ))}
             </div>
