9cf5a58995d8abcf1da4d86c4bcc7c992ee7c0f2
vas1levnv <vas1levnv@yandex.ru>
2023-03-23 17:29:57 +0300

    Пагинация сломанная

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,22 +1,44 @@
 import React, {useEffect, useState} from 'react';
 import s from './Posts.module.scss'
-import axios from "axios";
 
 const Posts = () => {
 
     const [posts, setPosts] = useState(null)
-    useEffect(() => {
+    const perPage = 3
+    const pages = []
+    const [currentPage, setCurrentPage] = useState('1')
+    const [totalUsers, setTotalUsers] = useState('')
 
-        axios.get('https://reqres.in/api/users',{
-        })
-            .then(response => response.data.data)
-            .then(response => setPosts(response))
+    const url = `https://reqres.in/api/users?page=${currentPage}&per_page=${perPage}`
+
+    const fetchPosts = async () => {
+        const data = await fetch(url)
+        const json = await data.json();
+        setTotalUsers(json.total_pages)
+        setPosts(json.data)
+    }
+
+    useEffect(() => {
+        fetchPosts()
+            .catch(console.error)
+
     }, [])
+
+    if (totalUsers) {
+        for (let i = 1; totalUsers + 1 > i; i++) {
+            pages.push(i)
+        }
+    }
 
     const removePost = (p) => {
         setPosts(posts.filter((post) => post.id !== p))
     }
 
+    const onPageItem = (e) => {
+        console.log(e.target.innerText)
+        setCurrentPage(e.target.innerText)
+        fetchPosts().catch()
+    }
 
     return (
         <div className='mt-5'>
@@ -32,6 +54,17 @@ const Posts = () => {
                     </div>
                 ))}
             </div>
+            <nav aria-label="Page navigation example">
+                <ul className="pagination">
+                    {/* <li className="page-item"><a className="page-link" href="#">Previous</a></li>*/}
+                    {pages && pages.map(page => (
+                        <li key={page} onClick={onPageItem} className="page-item"><span
+                            className="page-link">{page}</span></li>
+                    ))}
+
+                    {/* <li className="page-item"><a className="page-link" href="#">Next</a></li>*/}
+                </ul>
+            </nav>
         </div>
     );
 };
